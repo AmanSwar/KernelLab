c2399d67086cf33c25fbddaf36cfcd0da094bf1f
Aman <aman.swar001@gmail.com>
2025-02-28 16:28:40 +0530

    setup

---
+++ vector_add/python/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension , CUDAExtension
+
+setup(
+    name="vec_add_kernels",
+    ext_modules=[
+        CUDAExtension(
+            'vec_add_kernels',
+            sources=['python/binding.cpp' , 'src/naive_vecadd.cu'],
+            include_dirs=['include']
+        )
+    ],
+    cmdclass={"build_ext" : BuildExtension}
+)
+
