0ebf58e28a1072fb30efecc81648a1f2f484acd4
Aman <aman.swar001@gmail.com>
2025-03-02 03:13:39 +0530

    compare b/w torch and kernel

--- vector_add/python/vec_add.py
+++ vector_add/python/vec_add.py
@@ -1,5 +1,7 @@
-import torch
 import vec_add_kernels
+import torch
+
+
 
 a = torch.rand(size=(1000000,1) , device="cuda")
 b = torch.rand(size=(1000000,1) , device="cuda")
@@ -21,13 +23,31 @@ def main():
     import time
 
 
-    st = time.monotonic()
-    c = vec_add_kernels.naive_vec_add(a , b)
+    # st = time.monotonic()
+    # c = vec_add_kernels.naive_vec_add(a , b)
 
-    et = time.monotonic() - st
-    print(f"CUDA kernel execution time: {et:.6f} seconds")
-    checker(c)
+    # et = time.monotonic() - st
+    # print(f"CUDA kernel execution time: {et:.6f} seconds")
+    # checker(c)
 
+    total_time_kernel = 0
+    total_time_torch = 0
+    for i in range(5):
+        st_1 = time.monotonic()
+        c_1 = vec_add_kernels.multielement_vec_add(a ,b)
+        et_1 = time.monotonic() - st_1
+        print(f"CUDA kernel execution time (multielement): {et_1:.6f} seconds")
+        total_time_kernel += et_1
+
+        st = time.monotonic()
+        c = torch.add(a , b)
+        et = time.monotonic() - st
+        print(f"CUDA kernel execution time (pytorch): {et:.6f} seconds")
+        total_time_torch += et
+
+    print("torch : " , total_time_torch / 5)
+    print("kernel : " , total_time_kernel / 5)
+    print(c_1 == c)
 if __name__ == "__main__":
     main()
 
