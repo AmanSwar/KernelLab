b981e0b4a5c749bc1bdcfe6e28884f021ba042ce
Aman <aman.swar001@gmail.com>
2025-03-02 03:13:23 +0530

    added the most optim kernel

--- vector_add/python/setup.py
+++ vector_add/python/setup.py
@@ -1,15 +1,30 @@
+# from setuptools import setup
+# from torch.utils.cpp_extension import BuildExtension , CUDAExtension
+
+# setup(
+#     name="vec_add_kernels",
+#     ext_modules=[
+#         CUDAExtension(
+#             'vec_add_kernels',
+#             sources=['python/binding.cpp' , 'src/naive_vecadd.cu' , "src/multielement_vecadd.cu"],
+#             include_dirs=['include']
+#         ),
+#     ],
+#     cmdclass={"build_ext" : BuildExtension}
+# )
+
+
 from setuptools import setup
-from torch.utils.cpp_extension import BuildExtension , CUDAExtension
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
-    name="vec_add_kernels",
+    name='vec_add_kernels',
     ext_modules=[
         CUDAExtension(
             'vec_add_kernels',
-            sources=['python/binding.cpp' , 'src/naive_vecadd.cu'],
-            include_dirs=['include']
+            sources=['binding.cpp', '../src/naive_vecadd.cu', '../src/multielement_vecadd.cu'],
+            include_dirs=['../include']
         )
     ],
-    cmdclass={"build_ext" : BuildExtension}
+    cmdclass={'build_ext': BuildExtension}
 )
-
