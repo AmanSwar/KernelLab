bd2f251215cdc0a7cefb659baf33bd10ff121b48
Aman <aman.swar001@gmail.com>
2025-03-04 16:17:30 +0530

    updated header

--- img_process/greyscale/include/greyscale_kernel.h
+++ img_process/greyscale/include/greyscale_kernel.h
@@ -1,7 +1,20 @@
+
 #ifndef GREYSCALE_KERNEL_H
 #define GREYSCALE_KERNEL_H
+
 
 void launch_naive(const float *input , float *output , int width , int height , int channels);
 
 void launch_coal(const float *input , float *output , int width , int height , int channels);
+
+void launch_multipixel(const float *input , float *output , int width , int height , int channels , int pixelsPerThread);
+
+void launch_fma(const float *input , float *output , int width , int height);
+
+void launch_shared(const float *input , float *output , int width , int height , int channels);
+
+void launch_vectorized(const float4 *input , float *output , int width , int height);
+
+
+
 #endif
