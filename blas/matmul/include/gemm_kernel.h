b68b8668aebc249312b7a9a08be6b5ec68e1f914
Aman <aman.swar001@gmail.com>
2025-03-09 18:32:46 +0530

    added tensor cores

--- blas/matmul/include/gemm_kernel.h
+++ blas/matmul/include/gemm_kernel.h
@@ -1,6 +1,8 @@
 #ifndef GEMM_KERNEL_H
 #define GEMM_KERNEL_H
-
+#include <cuda_runtime.h>
+#include <cuda_fp16.h> 
+#include <mma.h>
 void launch_gemm_naive(float *d_A , float *d_B , float *d_C , float alpha , float beta , int M , int N , int K);
 
 void launch_gemm_tiled(float *d_A, float *d_B, float *d_C, float alpha, float beta, int M, int N, int K);
@@ -15,6 +17,6 @@ void launch_gemm_regblock(float *d_A, float *d_B, float *d_C, float alpha, float
 
 void launch_gemm_warp(float *d_A, float *d_B, float *d_C, int M, int N, int K);
 
-
+void launch_tensor_core_gemm(half* d_A, half* d_B, float* d_C, float alpha, float beta, int M, int N, int K);
 
 #endif
