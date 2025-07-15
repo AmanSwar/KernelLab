804ee68663b13de9baaeba641b28a5ca06283775
Aman <aman.swar001@gmail.com>
2025-07-15 06:16:56 +0530

    vectorized kernel mod - intake float* values*

--- blas/vector_add/src/vectorized_vecadd.cu
+++ blas/vector_add/src/vectorized_vecadd.cu
@@ -1,22 +1,37 @@
+#include <cstdlib>
 #include <cuda_runtime.h>
+#include <float.h>
+
+
 #include "../include/vec_add_kernel.h"
+#include "util.cpp"
 
-__global__
-void vectorized_vecadd(float4 *a , float4 *b , float4 *c , int n){
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    
-    if(i < n/4){
-        float4 a_i = a[i];
-        float4 b_i = b[i];
-        float4 c_i;
 
-        c_i.x  = a_i.x + b_i.x;
-        c_i.y = a_i.y + b_i.y;
-        c_i.z = a_i.z + b_i.z;
-        c_i.w = a_i.w + b_i.w;
+//float to float4 conversion via memory address : 
+//access mem address -> ask the compiler to treat the mem add as float4 -> return the value
+#define FLOAT4(value) (*reinterpret_cast<float4 *>(&(value)))
 
-        c[i] = c_i;
+__global__ void vector_add_f32_vectorized(
+    float* a,
+    float* b,
+    float* c,
+    int N
+){
+    //idx => multiple of 4 (0 , 4 , 8)
+    int idx = 4 * (blockIdx.x + blockDim.x + threadIdx.x);
 
+    if (idx < N){
+        // access float -> convert and store it in reg
+        float4 reg_a = FLOAT4(a[idx]);
+        float4 reg_b = FLOAT4(b[idx]);
+        float4 reg_c; 
+
+        reg_c.x = reg_a.x + reg_b.x;
+        reg_c.y = reg_a.y + reg_b.y;
+        reg_c.z = reg_a.z + reg_b.z;
+        reg_c.w = reg_a.w + reg_b.w;
+        // store float 4
+        FLOAT4(c[idx]) = reg_c;
     }
 }
 
@@ -24,8 +39,9 @@ void vectorized_vecadd(float4 *a , float4 *b , float4 *c , int n){
 void launchVectorized(float *a , float *b , float *c , int size){
     int blockSize = 256;
     int gridSize = (size / 4 + blockSize - 1) / blockSize;
-    vectorized_vecadd<<<gridSize , blockSize>>>((float4 *)a, (float4*)b, (float4*)c, size);
+    vector_add_f32_vectorized<<<gridSize, blockSize>>>(a, b , c, size);
     cudaDeviceSynchronize();
 }
+
 
 
