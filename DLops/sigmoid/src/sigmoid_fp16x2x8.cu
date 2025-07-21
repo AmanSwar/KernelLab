7909f66176fe1db3aebc496820f455fd85bedee5
Aman <aman.swar001@gmail.com>
2025-07-21 18:02:33 +0530

    add sigmoid fp16 vect revised kernel

--- DLops/sigmoid/src/sigmoid_fp16x2x8.cu
+++ DLops/sigmoid/src/sigmoid_fp16x2x8.cu
@@ -1,5 +1,6 @@
 #include <cuda_fp16.h>
 #include <cuda_runtime.h>
+#include <exception>
 
 #include "util.cuh"
 
@@ -35,9 +36,43 @@ __global__ void sigmoid_fp16x2x8_kernel(__half *input, __half *output, int N) {
 
 
 
-// __global__ void sigmoid_fp16x2x8_revised_kernel(__half* input , __half* output , int N){
-//   int global_idx = 
-// }
+__global__ void sigmoid_fp16x2x8_revised_kernel(__half* input , __half* output , int N){
+  const int elements_per_thread = 16; // reduce register pr.
+  
+  int idx = (blockIdx.x * blockDim.x + threadIdx.x) * elements_per_thread;  // 0 , el , el*2 ...
+ 
+  //check boudns
+  if(idx >= N){
+    return;
+  }
+
+  
+  const half2 one = __float2half2_rn(1.0f);
+
+  //pointer of type half2 to get input as half2 : on fly conversion is expensive as done above
+  half2* input_vec = reinterpret_cast<half2*>(input);
+  half2* output_vec = reinterpret_cast<half2*>(output);
+
+  int vec_idx = idx / 2;
+  //total vectors to be loaded (vec size = 2)
+  const int vec_per_thread = elements_per_thread / 2; 
+
+  #pragma unroll
+  for(int i = 0; i < vec_per_thread ; i++){
+    int curr_vec_idx = vec_idx + i;
+
+    if(curr_vec_idx * 2 < N){
+      half2 reg_a = __ldcg(&input_vec[curr_vec_idx]);
+
+      half2 _neg_a = __hneg2(reg_a); // negation instead of mul with -1
+      half2 _exp_neg_a = h2exp(_neg_a); 
+      half2 denom = __hadd2(one , _exp_neg_a);
+      half2 reg_b = __h2div(one , denom);
+
+      __stcg(&output_vec[curr_vec_idx] , reg_b);
+    }
+  }
+}
 
 void launch_sigmoid_kernel(__half *input, __half *output, int N) {
   int block_dim = 1024;
@@ -46,9 +81,20 @@ void launch_sigmoid_kernel(__half *input, __half *output, int N) {
   sigmoid_fp16x2x8_kernel<<<grid_dim, block_dim>>>(input, output, N);
 }
 
+void launch_revised_kernel(__half* input , __half* output , int N){
+  const int el_per_thread = 16;
+  const int block_dim = 256;
+  int threads_needed = (N + el_per_thread - 1) / el_per_thread;
+  int grid_dim = (threads_needed + block_dim -1) / block_dim;
+  const int num_sm = 16;
+  int grid_dim_padded = (grid_dim + num_sm - 1) / num_sm * num_sm;
+  sigmoid_fp16x2x8_revised_kernel<<<grid_dim_padded, block_dim>>>(input, output,
+                                                                  N);
+}
+
 
 int main() {
-  int N = 1000000;
+  long int N = 1000000;
   int iter = 100;
-  sigmoid::runBenchmark<half>(launch_sigmoid_kernel, N, iter);
+  sigmoid::runBenchmark<half>(launch_revised_kernel, N, iter);
 }
