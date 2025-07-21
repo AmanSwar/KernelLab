15fbca436a53de85f20f3416a3391387fcd66349
Aman <aman.swar001@gmail.com>
2025-07-21 08:40:24 +0530

    add support for fp16

--- DLops/sigmoid/src/util.cuh
+++ DLops/sigmoid/src/util.cuh
@@ -4,9 +4,11 @@
 #include <cuda_runtime.h>
 #include <cudnn_graph.h>
 #include <cudnn_ops.h>
+#include <cuda_fp16.h>
 
 #include <iostream>
-
+#include <type_traits>
+ 
 #define CHECK_CUDA(call)                                                       \
   {                                                                            \
     const cudaError_t error = call;                                            \
@@ -28,9 +30,12 @@
   }
 
 
+
+
+
 namespace sigmoid{
-// template <class dtype>
-void cudnn_sigmoid(float* input , float* output , int size , int iter=100){
+template <class dtype>
+void cudnn_sigmoid(dtype* input , dtype* output , int size , int iter=100){
 
     int n = 1 , c=1 , h=1;
 
@@ -39,7 +44,10 @@ void cudnn_sigmoid(float* input , float* output , int size , int iter=100){
 
     cudnnTensorDescriptor_t tensorDesc;
     cudnnCreateTensorDescriptor(&tensorDesc);
-    cudnnSetTensor4dDescriptor(tensorDesc, CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, n,  c,  h, size);
+    
+    auto cudnn_dtype = (std::is_floating_point<dtype>::value) ? (CUDNN_DATA_FLOAT) : (CUDNN_DATA_HALF);
+    cudnnSetTensor4dDescriptor(tensorDesc, CUDNN_TENSOR_NCHW, cudnn_dtype, n, c,
+                               h, size);
 
     cudnnActivationDescriptor_t sigmoidDesc;
     cudnnCreateActivationDescriptor(&sigmoidDesc);
@@ -86,40 +94,55 @@ void cudnn_sigmoid(float* input , float* output , int size , int iter=100){
     cudnnDestroy(handle);
 }
 
-inline void init_arr(float *a, int N) {
+
+template <class dtype>
+inline void init_arr(dtype *a, int N) {
   for (int i = 0; i < N; i++) {
     a[i] = rand();
   }
 }
 
 
-inline bool verify(float* kernel_out , float* out , int N , float err = 0.0001){
 
+inline bool verify_float(float* kernel_out , float* out , int N , float err = 0.0001){
     for(int i = 0 ; i < N ; i++){
         if(std::abs(kernel_out[i] - out[i]) > err){
             return false;
         }
     }
     return true;
-
-
 }
 
-void runBenchmark(void(*kernel)(float* , float* , int) , int N , int iter){
+inline bool verify_half(half *kernel_out, half *out, int N,
+                         half err = 0.0001) {
+  for (int i = 0; i < N; i++) {
+    if (__habs(kernel_out[i] - out[i]) > err) {
+      std::cout << i << std::endl;
+      printf("%f \n" , __half2float(kernel_out[i]));
+      printf("%f \n" , __half2float(out[i]));
 
-    float* i = new float[N];
-    float* o = new float[N];
-    float* cudnn_o = new float[N];
+      return false;
+    }
+  }
+  return true;
+}
+
+template <class dtype>
+void runBenchmark(void(*kernel)(dtype* , dtype* , int) , int N , int iter){
+
+    dtype* i = new dtype[N];
+    dtype* o = new dtype[N];
+    dtype* cudnn_o = new dtype[N];
 
     init_arr(i , N);
 
-    float* input , *output , *cudnn_output;
+    dtype* input , *output , *cudnn_output;
 
-    cudaMalloc(&input , N * sizeof(float));
-    cudaMalloc(&output , N * sizeof(float));
-    cudaMalloc(&cudnn_output , N * sizeof(float));
+    cudaMalloc(&input , N * sizeof(dtype)) ;
+    cudaMalloc(&output , N * sizeof(dtype));
+    cudaMalloc(&cudnn_output , N * sizeof(dtype));
 
-    cudaMemcpy(input , i , N * sizeof(float) , cudaMemcpyHostToDevice);
+    cudaMemcpy(input , i , N * sizeof(dtype) , cudaMemcpyHostToDevice);
     std::cout << "Running cudnn sigmoid" << std::endl;
     cudnn_sigmoid(input , cudnn_output , N , iter=iter);
 
@@ -142,10 +165,17 @@ void runBenchmark(void(*kernel)(float* , float* , int) , int N , int iter){
     std::cout << "Kernel TIME : " << ms / iter << std::endl;
     std::cout << "Kernel GFLOP : " << (N / (ms / 1000)) / 1e9 << std::endl;
 
-    cudaMemcpy(o , output , N * sizeof(float) , cudaMemcpyDeviceToHost);
-    cudaMemcpy(cudnn_o ,cudnn_output  , N * sizeof(float) , cudaMemcpyDeviceToHost);
+    cudaMemcpy(o , output , N * sizeof(dtype) , cudaMemcpyDeviceToHost);
+    cudaMemcpy(cudnn_o ,cudnn_output  , N * sizeof(dtype) , cudaMemcpyDeviceToHost);
 
     //verify
-    std::cout << verify(o , cudnn_o , N) << std::endl;
+    bool result;
+    if(std::is_floating_point<dtype>::value) {
+      // result = verify_float(o, cudnn_o, N);
+     }else{
+      result = verify_half(o, cudnn_o, N);
+     }
+
+    std::cout << result << std::endl;
 }
 }
