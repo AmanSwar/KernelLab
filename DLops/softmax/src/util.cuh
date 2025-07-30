8b676f575368e370461c8707f994ed2cb331167a
Aman <aman.swar001@gmail.com>
2025-07-30 21:16:47 +0530

    add reduce sum ops

--- DLops/softmax/src/util.cuh
+++ DLops/softmax/src/util.cuh
@@ -101,7 +101,59 @@ __device__ float block_max_fp32x4(float *arr , int N){
     return block_max_smem;
 }
 
+template <const int kWarpSize = WARP_SIZE>
+__device__ __forceinline__ float warp_reduce_sum_f32(float val) {
+#pragma unroll
+  for (int mask = kWarpSize >> 1; mask >= 1; mask >>= 1) {
+    val += __shfl_xor_sync(0xffffffff, val, mask);
+  }
+  return val;
+}
 
+template <const int NUM_THREADS = 256 >
+__device__ float block_all_reduce_f32x4_kernel(
+    float *input ,
+    float* output,
+    int N
+){
+    int tid = threadIdx.x;
+    int global_idx = (blockDim.x * blockIdx.x + threadIdx.x)*4;
+
+    constexpr int NUM_WARPS = (NUM_THREADS + WARP_SIZE - 1) / WARP_SIZE;
+    __shared__ float reduce_smem[NUM_WARPS];
+    
+    // float4 reg_a = FLOAT4(input[global_idx]);
+    float4 reg_a;
+    
+    
+    if (global_idx + 3 < N) {
+      reg_a = FLOAT4(input[global_idx]);
+    } else {
+      // Handle tail end of the data safely.
+      reg_a.x = (global_idx < N) ? input[global_idx] : 0.0f;
+      reg_a.y = (global_idx + 1 < N) ? input[global_idx + 1] : 0.0f;
+      reg_a.z = (global_idx + 2 < N) ? input[global_idx + 2] : 0.0f;
+      reg_a.w = (global_idx + 3 < N) ? input[global_idx + 3] : 0.0f;
+    }
+    float sum = (global_idx + 3 < N) ? (reg_a.x + reg_a.y + reg_a.z + reg_a.w) : 0.0f;
+    int warp = tid / WARP_SIZE;
+    int lane = tid % WARP_SIZE;
+
+    sum = warp_reduce_sum_f32<WARP_SIZE>(sum);
+
+    if(lane == 0){
+        reduce_smem[warp] = sum;
+    }
+
+    __syncthreads();
+
+    sum = (lane < NUM_WARPS) ? reduce_smem[lane] : 0.0f;
+
+    if(warp == 0){
+        return warp_reduce_sum_f32<WARP_SIZE>(sum);
+    }
+    
+}
 
 
 
