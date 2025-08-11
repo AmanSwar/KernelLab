28133b248da4fe5d76919a074bb4358e3d416b27
Aman <aman.swar001@gmail.com>
2025-08-11 15:47:13 +0530

    add(utils) : warp reduce for fp32 , fp16 , mixed precision and block reduce for fp16

--- DLops/rmsnorm/include/util.cuh
+++ DLops/rmsnorm/include/util.cuh
@@ -1,14 +1,64 @@
+#include <__clang_cuda_builtin_vars.h>
+#include <__clang_cuda_runtime_wrapper.h>
 #include <cuda_runtime.h>
 #include <cuda_fp16.h>
 
-template <const int WARP_SIZE = 32>
+#define WARP_SIZE 32
+
 __device__ __forceinline__ half warp_reduce_sum_fp16(half val){
-    const int mask = 0xffffffff;
+    constexpr int MASK = 0xffffffff;
     #pragma unroll
     for(int offset = WARP_SIZE >> 1 ; WARP_SIZE >= 1 ; offset >>= 1){
-        val += __shfl_xor_sync(mask ,val ,  offset);
+        val += __shfl_xor_sync(MASK ,val ,  offset);
     }
     return val;
 }
 
 
+/*
+Warp reduce function to accumilate in fp32 for better precision
+*/
+__device__ __forceinline__ float warp_reduce_sum_fp16_fp32(half val){
+    constexpr int MASK = 0xffffffff;
+
+    float val_fp32 = __half2float(val);
+    #pragma unroll 
+    for(int offset = WARP_SIZE >> 1 ; offset >= 1 ; offset >>= 1){
+      val_fp32 += __shfl_xor_sync(MASK, val_fp32, offset);
+    }
+    return val_fp32;
+}
+
+__device__ __forceinline__ float warp_reduce_sum_f32(float val) {
+  constexpr int MASK = 0xffffffff;
+#pragma unroll
+  for (int offset = WARP_SIZE >> 1; offset >= 1; offset >>= 1) {
+    val += __shfl_xor_sync(MASK, val, offset);
+  }
+  return val;
+}
+
+
+template <const int NUM_THREADS = 256>
+__device__ __forceinline__ float block_reduce_sum_fp16_fp16(half val) {
+
+  int local_index_x = threadIdx.x;
+  constexpr int NUM_WARPS = (NUM_THREADS + WARP_SIZE - 1) / WARP_SIZE;
+  int warp_id = local_index_x / WARP_SIZE;
+  int lane_id = local_index_x % WARP_SIZE;
+
+  static __shared__ float smem[NUM_WARPS];
+
+  float val_fp32 = warp_reduce_sum_fp16_fp32(val);
+
+  if(lane_id == 0){
+    smem[warp_id] = val_fp32;
+  }
+
+  __syncthreads();
+
+  val_fp32 = (lane_id < NUM_WARPS) ? smem[lane_id] : 0.0f;
+  val_fp32 = warp_reduce_sum_f32(val_fp32);
+
+  return val_fp32;
+}
