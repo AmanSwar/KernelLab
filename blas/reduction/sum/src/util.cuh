44808f1d7cbbdf5def918867356700a4477aa785
Aman <aman.swar001@gmail.com>
2025-08-20 16:55:35 +0530

    add(util) : add device function warp reduce sum fp16

--- blas/reduction/sum/src/util.cuh
+++ blas/reduction/sum/src/util.cuh
@@ -1,6 +1,9 @@
 
+#include <cstdlib>
 #include <cuda_runtime.h>
 #include <iostream>
+#include <cuda_fp16.h>
+#include <sys/types.h>
 
 #define WARP_SIZE 32
 
@@ -16,13 +19,34 @@ __device__ __forceinline__ float warp_reduce_sum_f32(float val){
     return val;
 }
 
+__device__ __forceinline__ half warp_reduce_sum_fp16(half val){
+    const uint mask = 0xffffffff;
+    #pragma unroll 
+    for(int offset = 32 >> 1 ; offset >= 1 ; offset >>= 1){
+        val += __shfl_xor_sync(mask , val , offset);
+    }
+
+    return val;
+}
+
 
 template <class dtype>
 inline void init(dtype* a , int N){
     for(int i = 0 ; i < N ; i++){
-        a[i] = rand();
+        a[i] = i;
     }
 }
+
+
+float inline cpu_sum(float* arr , int N){
+    float sum = 0;
+
+    for(int i = 0 ; i < N ; i++){
+        sum += arr[i];    
+    }
+    return sum;
+}
+
 
 
 template <class dtype>
@@ -35,6 +59,7 @@ void reduceBenchmark(void(*kernel)(dtype* , dtype* , int ) , int N , int iter){
     cudaMalloc(&b , N * sizeof(dtype));
 
     init<dtype>(ha , N);
+    float cpu_out = cpu_sum(ha, N);
     cudaMemcpy(a , ha , sizeof(float) * N , cudaMemcpyHostToDevice);
 
     cudaEvent_t start , stop;
@@ -52,11 +77,13 @@ void reduceBenchmark(void(*kernel)(dtype* , dtype* , int ) , int N , int iter){
     float ms = 0;
     cudaEventElapsedTime(&ms , start , stop);
 
+    cudaMemcpy(hb , b , sizeof(float) * N , cudaMemcpyDeviceToHost);
+
     std::cout << "Kernel Time : " << ms / iter << std::endl;
-    std::cout << "Kernel GFLOP : " << (2*N / (ms / 1000)) / 1e9 << std::endl;
+    std::cout << "Kernel GFLOP : " << (2*N / (ms / iter/ 1000)) / 1e9 << std::endl;
 
-    
-
+    std::cout << hb[0] << " " << cpu_out << std::endl; 
+    std::cout << (hb[0] == cpu_out) << std::endl;
 
 
 
