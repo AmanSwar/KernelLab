5cdc6798aec5bcfb78b4d850e834c4a16d447e1d
Aman <aman.swar001@gmail.com>
2025-09-17 10:02:18 +0530

    feat : add bf16 warp reduce and block reduce

--- CUDA/DLops/rmsnorm/include/util.cuh
+++ CUDA/DLops/rmsnorm/include/util.cuh
@@ -1,14 +1,17 @@
 #include <__clang_cuda_builtin_vars.h>
 #include <__clang_cuda_runtime_wrapper.h>
+#include <cstdint>
 #include <cuda_runtime.h>
 #include <cuda_fp16.h>
+#include <cuda_bf16.h>
 
 #define WARP_SIZE 32
+
 
 __device__ __forceinline__ half warp_reduce_sum_fp16(half val){
     constexpr int MASK = 0xffffffff;
     #pragma unroll
-    for(int offset = WARP_SIZE >> 1 ; WARP_SIZE >= 1 ; offset >>= 1){
+    for(int offset = WARP_SIZE >> 1 ; offset >= 1 ; offset >>= 1){
         val += __shfl_xor_sync(MASK ,val ,  offset);
     }
     return val;
@@ -61,4 +64,44 @@ __device__ __forceinline__ float block_reduce_sum_fp16_fp16(half val) {
   val_fp32 = warp_reduce_sum_f32(val_fp32);
 
   return val_fp32;
+}
+
+
+__device__ __forceinline__ nv_bfloat16 warp_reduce_sum_bf16(nv_bfloat16 val){
+  constexpr uint64_t MASK = 0xffffffff;
+  #pragma unroll
+  for(int offset = MASK >> 1 ; offset >= 1 ; offset >>= 1){
+    val += __shfl_xor_sync(MASK , val , offset);
+  }
+  return val;
+}  
+
+//function to reduce sum in a single block -> stored in smem
+__device__ __forceinline__ nv_bfloat16 block_reduce_sum_bf16(
+  nv_bfloat16* smem_ptr,
+  nv_bfloat16* temp_smem_ptr
+){
+  int idx = threadIdx.x;
+
+  int warp_id = idx / 32;
+  int lane_id = idx % 32;
+
+  nv_bfloat16 value = smem_ptr[idx];
+
+  const int NUM_WARPS = (blockDim.x + WARP_SIZE - 1 / WARP_SIZE);
+
+  value = warp_reduce_sum_bf16(value);
+
+  if(lane_id == 0){
+    temp_smem_ptr[warp_id] = value;
+  }
+
+  __syncthreads();
+
+
+  value = (lane_id < NUM_WARPS) ? temp_smem_ptr[lane_id] : 0.0f;
+  value = warp_reduce_sum_bf16(value);
+
+  return value;
+
 }
