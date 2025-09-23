be129e5f44fe44504f801c351eb6b51409914915
Aman <aman.swar001@gmail.com>
2025-09-23 09:19:33 +0530

    setup py file for rope

---
+++ CUDA/DLops/rope/python/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension , CUDAExtension
+
+setup(
+    name="rope_cuda",
+    ext_modules=[
+        CUDAExtension(
+            "rope_cuda",
+            sources=["src/binding.cu"],
+            extra_compile_args={
+                "cxx": [],
+                "nvcc": [
+                    "-O3",
+                    "-gencode",
+                    "arch=compute_80,code=sm_80",
+                    "--expt-relaxed-constexpr",
+                ],
+            },
+        )
+    ],
+    cmdclass={"build_ext": BuildExtension},
+)
